fcfc0bd70b4a47c0603594a90bbacf3f7f5c0801
Hamza Bettal <hbettal@e3r7p10.1337.ma>
2024-08-06 16:13:26 +0100

    fix hange problem when losing

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById('gameCanvas');
+console.log(canvas);
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -18,51 +19,68 @@ const words = ["cat", "dog", "bird", "fish", "tree", "house", "car", "bike", "ro
 let asteroids = [];
 let inputWord = "";
 
-const spawnInterval = 2000;
-const asteroidSpeed = 1;
+const spawnlevel = 2000;
+const wordspeed = 1;
 const asteroidFrequency = 2000;
 const wordInput = document.getElementById('input_word');
 
-class Asteroid {
-    constructor(word, x, y, speed) {
+class Asteroid
+{
+    constructor(word, x, y, speed)
+    {
         this.word = word;
         this.x = x;
         this.y = y;
         this.speed = speed;
     }
-
-    draw() {
+    draw()
+    {
         ctx.font = '24px Arial';
         ctx.fillStyle = '#fff';
         ctx.fillText(this.word, this.x, this.y);
     }
-
-    update() {
+    update()
+    {
         this.y += this.speed;
     }
 }
 
-function spawnAsteroid() {
+function spawnAsteroid()
+{
     const word = words[Math.floor(Math.random() * words.length)];
     const x = Math.random() * (canvas.width - 50);
-    asteroids.push(new Asteroid(word, x, -50, asteroidSpeed));
+    asteroids.push(new Asteroid(word, x, -80, wordspeed));
 }
 
-function updateGame() {
+function game_intro()
+{
+    
+}
+
+function updateGame()
+{
+    let i = 0;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    asteroids.forEach((asteroid, index) => {
+    while (i < asteroids.length)
+    {
+        const asteroid = asteroids[i];
         asteroid.update();
         asteroid.draw();
-        if (asteroid.word === inputWord.trim()) {
-            asteroids.splice(index, 1);
+        if (asteroid.word === inputWord.trim())
+        {
+            asteroids.splice(i, 1);
             inputWord = "";
             wordInput.value = "";
+            i--;
         }
-        if (asteroid.y > canvas.height) {
+        if (asteroid.y > canvas.height)
+        {
             alert('Game Over!');
             document.location.reload();
+            return;
         }
-    });
+        i++;
+    }
     requestAnimationFrame(updateGame);
 }
 
@@ -70,5 +88,5 @@ wordInput.addEventListener('input', (e) => {
     inputWord = e.target.value;
 });
 
-setInterval(spawnAsteroid, spawnInterval);
+setInterval(spawnAsteroid, spawnlevel);
 updateGame();
